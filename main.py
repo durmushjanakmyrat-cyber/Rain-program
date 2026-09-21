685645aedaf928d7dcd9e90ea0f0d6e146456298
durmushjanakmyrat-cyber <durmushjanakmyrat@gmail.com>
2026-09-21 22:04:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ app = FastAPI()
 # ---------------------------------------------------------
 # НАСТРОЙКИ (Вставьте свои данные)
 # ---------------------------------------------------------
-WEATHER_API_KEY = "ВАШ_OPENWEATHER_API_KEY"
-TELEGRAM_BOT_TOKEN = "ВАШ_TELEGRAM_BOT_TOKEN"
-ADMIN_PIN = "1234"  # ПИН-код для входа в панель владельца
+WEATHER_API_KEY = "2de4b7fbe5dd6de7e15810555d61457f"
+TELEGRAM_BOT_TOKEN = "8539880858:AAH-LroXnwOpZq4v8p-qmnhDOkd3thDaWIA"
+ADMIN_PIN = "122595"  # ПИН-код для входа в панель владельца
 
 PHENOMENA = {
     "rain": {"name": "🌧️ Дождь", "min_id": 200, "max_id": 531},
